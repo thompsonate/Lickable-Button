a58e4cccf504b3fe852b398f7ac40277ae2566ab
thompsonate <nate@natethompson.io>
2021-06-09 14:55:48 -0400

    Initial Commit

---
+++ LickableButton/LickableButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LickableButtonApp.swift
+//  LickableButton
+//
+//  Created by Nate Thompson on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LickableButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
